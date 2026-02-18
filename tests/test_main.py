98e704021ebe754305736c11cb2709c62ff732af
Jonah Rothman <jonahrothman@me.com>
2026-02-17 20:00:34 -0500

    Fixes to Main and Tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 import pytest
-from app.main import add, app
+from app.main import add, app, hello
 
 def test_add_positive():
   assert add(2, 3) == 5
@@ -8,14 +8,7 @@ def test_add_negative():
   assert add(-1, -1) == -2
 
 def test_flask_hello():
-  client = app.test_client()
-  resp = client.get("/")
-  assert resp.status_code == 200
+  with app.test_request_context("/"):
+    resp = hello()
   data = resp.get_json()
   assert data["message"].startswith("Hello")
-
-def test_fail():
-  assert add(2, 2) == 5
-
-def test_fail_again():
-  assert add(2, 4) == 10
